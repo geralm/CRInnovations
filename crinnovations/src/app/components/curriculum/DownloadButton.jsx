55045d2da6f09116da97506103126b3e6523536d
geralm <estebanlm@estudiantec.cr>
2024-01-25 15:35:31 -0600

    Before <StyleEngineProvider> component

--- crinnovations/src/app/components/curriculum/DownloadButton.jsx
+++ crinnovations/src/app/components/curriculum/DownloadButton.jsx
@@ -1,15 +1,14 @@
 "use client";
 import Button from "@mui/material/Button";
 import DownloadIcon from "@mui/icons-material/Download";
-function DownloadButton() {
-  const handleClick = () => {
+function DownloadButton({...props}) {
+//   const handleClick = () => {
     // fs.writeFileSync('pdfs/someFile.pdf', anteproyecto.documento)
     // const pdfFilename = 'pdfs/someFile.pdf';
     // res.sendFile('pdfs/someFile.pdf', { root: './' })
-    console.log("Downloading CV");
-  };
+//   };
   return (
-    <Button variant="outlined" color="secondary" endIcon={<DownloadIcon />} onClick={handleClick} slot="rounded-full">
+    <Button {...props} variant="outlined" color="secondary" endIcon={<DownloadIcon />} slot="rounded-full">
       Download CV
     </Button>
   );
